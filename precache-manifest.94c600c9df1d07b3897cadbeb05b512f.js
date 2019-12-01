13f568edb6567ac4fbdb4e3838686a4be3736cc2
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-01 15:14:41 -0500

    Updates

---
+++ precache-manifest.94c600c9df1d07b3897cadbeb05b512f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c38b34f3408f0543b4a444c28b6cb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f442635d99ad2f3e99c1",
+    "url": "/static/css/main.2b2f38a4.chunk.css"
+  },
+  {
+    "revision": "3a913ec6be2ed69cf023",
+    "url": "/static/js/2.5e2070fa.chunk.js"
+  },
+  {
+    "revision": "f442635d99ad2f3e99c1",
+    "url": "/static/js/main.aff3bdba.chunk.js"
+  },
+  {
+    "revision": "2cf28e6d056999b0107d",
+    "url": "/static/js/runtime-main.017fa1fb.js"
+  },
+  {
+    "revision": "ad6970cd9721ee17b0f0c8dc7edabefa",
+    "url": "/static/media/bestBooks.ad6970cd.png"
+  },
+  {
+    "revision": "fed03568fe7755cde6e734c10aaf514c",
+    "url": "/static/media/hangman.fed03568.png"
+  },
+  {
+    "revision": "2a80f7414cd679a47f437c667d8a647b",
+    "url": "/static/media/profile.2a80f741.jpg"
+  },
+  {
+    "revision": "739b8ef7234e0bc5de608599fab0989f",
+    "url": "/static/media/topStories.739b8ef7.png"
+  }
+]);
